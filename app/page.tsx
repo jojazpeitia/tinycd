7ad931effa7bfde7f94f18b231e45470671dde8d
jojazpeitia <jojazpeitia@gmail.com>
2025-12-24 03:07:48 -0600

    fixed hint font size being too big

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ export default function Home() {
   return (
     <main style={{ width: '100vw', height: '100vh' }}>
       <input type="checkbox" id="hint-toggle" className="hint-toggle" />
+
       <div className="hint">
         <span>Click and drag to see from different angles</span>
         <label htmlFor="hint-toggle" className="hint-close"> x </label>
       </div>
+
       <Scene />
     </main>
   )
